0981bf2706e4b2064f31c78eafa6473b39170b54
ItsUntapped <88380127+mohaurammala@users.noreply.github.com>
2022-12-13 10:11:39 +0200

    Update App.tsx for CI/CD test

--- counsellorDMS-app/src/App.tsx
+++ counsellorDMS-app/src/App.tsx
@@ -15,7 +15,7 @@ function App() {
           <img src={reactLogo} className='logo react' alt='React logo' />
         </a>
       </div>
-      <h1>Vite + React, change for test</h1>
+      <h1>Vite + React, change for test 123</h1>
       <div className='card'>
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
