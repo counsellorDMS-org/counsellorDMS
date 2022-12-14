2e34ddf0ea9769a443709c8ab98c6b060870aeee
ItsUntapped <88380127+mohaurammala@users.noreply.github.com>
2022-12-14 10:15:50 +0200

    Added theme and coming soon page

---
+++ src/theme.ts
@@ -0,0 +1,13 @@
+import { createTheme } from "@mui/material/styles";
+
+export const theme = createTheme({
+  //App theme colors
+  palette: {
+    primary: {
+      main: "#B09A51",
+    },
+    secondary: {
+      main: "#161715",
+    },
+  },
+});
